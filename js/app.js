ebecc6972e817fc96ac1c75d1cabd590e645c83d
nstreetman <nicole.streetman@nicoles-air.attlocal.net>
2017-04-03 23:34:41 -0400

    bedtime

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
-function countdown() {
-    if(clock > 0) {
-        clock = clock - 1;
-        document.getElementById("timer").innerHTML = " ''<p>' + '.clock' + '</p>' ";
-    } else {
-        // Stop timer
-        clearInterval(countdownId);
-    }
-}
+// function countdown() {
+//     if(clock > 0) {
+//         clock = clock - 1;
+//         document.getElementById("timer").innerHTML = " ''<p>' + '.clock' + '</p>' ";
+//     } else {
+//         // Stop timer
+//         clearInterval(countdownId);
+//     }
+// }
+
+let submitButtonEl = document.querySelector (".submit");
+let taskInputForm = document.querySelector (".input");
+let timerEl = document.querySelector (".timer-box")
+let timerNumEl = document.querySelector (".timer-numerals")
+let timerCompleteEl = document.querySelector (".timer-complete")
+let taskBox = document.querySelector (".task-box")
+
+submitButtonEl.addEventListener(click, function(){
+  let taskInputForm = document.getElementById("task-input").value;
+  
+
+
+})
