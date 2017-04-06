bca169acbe356bdcfdb59043218855bbf37f38df
nstreetman <nicole.streetman@nicoles-air.attlocal.net>
2017-04-06 13:07:58 -0400

    timer is working but the task input and the styling is still jenky

--- js/app.js
+++ js/app.js
@@ -1,23 +1,40 @@
-// function countdown() {
-//     if(clock > 0) {
-//         clock = clock - 1;
-//         document.getElementById("timer").innerHTML = " ''<p>' + '.clock' + '</p>' ";
-//     } else {
-//         // Stop timer
-//         clearInterval(countdownId);
-//     }
-// }
-
 let submitButtonEl = document.querySelector (".submit");
 let taskInputForm = document.querySelector (".input");
-let timerEl = document.querySelector (".timer-box")
-let timerNumEl = document.querySelector (".timer-numerals")
-let timerCompleteEl = document.querySelector (".timer-complete")
-let taskBox = document.querySelector (".task-box")
+let timerEl = document.querySelector (".timer-box");
+let timerNumEl = document.querySelector (".timer-numerals");
+let timerCompleteEl = document.querySelector (".timer-complete");
+let taskBox = document.querySelector (".task-box");
 
-submitButtonEl.addEventListener(click, function(){
-  let taskInputForm = document.getElementById("task-input").value;
-  
+ function convertToMinutes(milliseconds) {
+  let totalTimeInSeconds = Math.floor(milliseconds/1000);
+  let timeInMinutes = Math.floor(totalTimeInSeconds/60);
+  let timeInSeconds = totalTimeInSeconds - timeInMinutes * 60;
+    if(timeInSeconds < 10) {
+      timeInSeconds = '0' + timeInSeconds;
+    }
+    return timeInMinutes + ":" + timeInSeconds
+}
 
+submitButtonEl.addEventListener('click', function(){
+  let theTaskInput = document.getElementById("task-input").value;
+  taskBox.innerHTML = theTaskInput;
+  taskInputForm.style.display = 'none';
+  let startingTimerValue = 1500000;
+  let timerValue = setInterval(function(){
+  startingTimerValue = startingTimerValue - 1000
+    if (startingTimerValue < 60000) {
+      timerEl.style.backgroundImage = "url(../images/tomato-red-silhouette.svg)";
+    }
+    if (startingTimerValue < 0){
+      clearInterval(timerValue)
+      timerNumEl.style.display = 'none';
+      timerCompleteEl.style.display = 'block';
+      taskBox.style.display = 'none';
+      timerEl.style.backgroundImage = "url(../images/tomato-red-silhouette.svg)";
+    }
+
+  let timeOnTimer = convertToMinutes(startingTimerValue)
+  timerEl.innerHTML = "<p class='timer-numerals'>" + timeOnTimer +"</p>";
+}, 1000)
 
 })
